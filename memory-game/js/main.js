0714f6cd13fd193290fd39068d185805adf19f54
lucymait <59390910+lucymait@users.noreply.github.com>
2020-01-23 21:41:32 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -24,21 +24,34 @@ const cards = [
 
 const cardsInPlay = [];
 
-function checkForMatch() {
+function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
 }
-function flipCard(cardId) {
+
+function flipCard() {
+	const cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank); 
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard (0);
-flipCard (2);
+
+function createBoard () {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard()
